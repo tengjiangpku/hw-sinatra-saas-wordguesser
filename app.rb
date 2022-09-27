6bad0097a3fe42cc46fcd699fd739743beeb4df9
Teng Jiang <tj2488@columbia.edu>
2022-09-27 05:28:44 +0000

    fix guess

--- app.rb
+++ app.rb
@@ -40,15 +40,19 @@ class WordGuesserApp < Sinatra::Base
   post '/guess' do
     letter = params[:guess].to_s[0]
     ### YOUR CODE HERE ###
-    # invalid
-    letter = letter.downcase
-    if letter.nil? or not letter.match(/^[a-zA-Z]$/)
+    if letter.nil?
       flash[:message] = "Invalid guess."
-    # repeated
-    elsif @game.guesses.include?(letter) or @game.wrong_guesses.include?(letter)
-      flash[:message] = "You have already used that letter."
     else
-      @game.guess(letter)
+      if not letter.match(/^[a-zA-Z]$/)
+        flash[:message] = "Invalid guess."
+      else
+        letter = letter.downcase
+        if @game.guesses.include?(letter) or @game.wrong_guesses.include?(letter)
+        flash[:message] = "You have already used that letter."
+        else
+          @game.guess(letter)
+        end
+      end
     end
     redirect '/show'
   end
