b2516029ee0fa04c5fc2db781317b98af6a6ad6f
Teng Jiang <tj2488@columbia.edu>
2022-09-27 05:16:25 +0000

    fix attempt_num

--- lib/wordguesser_game.rb
+++ lib/wordguesser_game.rb
@@ -4,7 +4,7 @@ class WordGuesserGame
   # to make the tests in spec/wordguesser_game_spec.rb pass.
 
   # Get a word from remote "random word" service
-  attr_accessor :word, :guesses, :wrong_guesses, :@attempt_num
+  attr_accessor :word, :guesses, :wrong_guesses, :attempt_num
   def initialize(word)
     @word = word
     @guesses = ''
